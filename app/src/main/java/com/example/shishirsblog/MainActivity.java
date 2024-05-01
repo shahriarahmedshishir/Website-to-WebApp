52364eeeb3e1d1d4d2599ae2c0d340d4c769b97d
shishir <shesirsikder1234@gmail.com>
2024-05-01 17:20:21 +0600

    Website to WebApp

---
+++ app/src/main/java/com/example/shishirsblog/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.shishirsblog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView myWeb;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myWeb=findViewById(R.id.myWeb);
+        myWeb.getSettings().setJavaScriptEnabled(true);
+        myWeb.setWebViewClient(new WebViewClient());
+        myWeb.loadUrl("https://shahriarshishirblog.netlify.app");
+
+    }
+}
